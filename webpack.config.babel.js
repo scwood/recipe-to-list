8f094fd25f611a2305f1072e1673883b73f0dd23
Spencer Wood <spencercwood@gmail.com>
2016-04-16 23:28:35 -0600

    Move to browserHistory, move away from redux

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,23 +3,23 @@ import path from 'path';
 import webpack from 'webpack';
 
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: path.join(__dirname,'src/client/index.html'),
+  template: path.join(__dirname, 'src/client/index.html'),
   filename: 'index.html',
-  inject: 'body'
+  inject: 'body',
 });
 
 export default {
   entry: [
-    path.join(__dirname, 'src/client/index.jsx'),
+    path.join(__dirname, 'src/client/index.js'),
     'webpack-dev-server/client?http://0.0.0.0:8080',
     'webpack/hot/only-dev-server',
   ],
   output: {
     path: path.join(__dirname, 'dist/static'),
-    filename: 'bundle.js',
+    filename: '/bundle.js',
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
   },
   module: {
     loaders: [
@@ -27,15 +27,15 @@ export default {
         test: /\.jsx?$/,
         loaders: ['react-hot', 'babel-loader'],
         include: path.join(__dirname, 'src/client'),
-      }
-    ]
+      },
+    ],
   },
   devServer: {
     proxy: {
       '/api/*': {
-        target: 'http://0.0.0.0:3000'
-      }
-    }
+        target: 'http://0.0.0.0:3000',
+      },
+    },
   },
-  plugins: [HTMLWebpackPluginConfig, new webpack.HotModuleReplacementPlugin()]
+  plugins: [HTMLWebpackPluginConfig, new webpack.HotModuleReplacementPlugin()],
 };
